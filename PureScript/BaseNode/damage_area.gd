260484b506efac2f9ddee9ef84e422c0e596569f
curiosity29 <domanhdung08042001@gmail.com>
2025-02-17 19:52:39 +0700

    quick changes dump

--- PureScript/BaseNode/damage_area.gd
+++ PureScript/BaseNode/damage_area.gd
@@ -5,15 +5,13 @@ extends Area2D
 @export var destroy_owner_hit: bool = false
 
 func _ready() -> void:
-    area_entered.connect(deal_damage)
+	area_entered.connect(deal_damage)
 
 
 func deal_damage(area: Area2D) -> void:
-    var area_onwer = area.owner
-    if area_onwer.has_method("take_damage"):
-        area_onwer.take_damage(damage)
+	var area_onwer = area.owner
+	if area_onwer.has_method("take_damage"):
+		area_onwer.take_damage(damage)
 
-    if destroy_owner_hit:
-        owner.queue_free()
-
-
+	if destroy_owner_hit:
+		owner.queue_free()
--- PureScript/GameObject/projectile.gd
+++ PureScript/GameObject/projectile.gd
@@ -6,17 +6,17 @@ var fire_direction: Vector2 = Vector2.ZERO
 
 
 func fire_self(direction) -> void:
-    fire_direction = direction
+	fire_direction = direction
 
 
 func _process(delta: float) -> void:
-    if fire_direction:
-        global_position += fire_direction * move_speed * delta
-    
-    check_out_of_screen()
+	if fire_direction:
+		global_position += fire_direction * move_speed * delta
+	
+	check_out_of_screen()
 
 
 
 func check_out_of_screen() -> void:
-    if min(global_position.x, global_position.y) <-1000 or max(global_position.x, global_position.y) > get_viewport_rect().size.x + 1000 :
-        queue_free()
+	if min(global_position.x, global_position.y) <-1000 or max(global_position.x, global_position.y) > get_viewport_rect().size.x + 1000 :
+		queue_free()
