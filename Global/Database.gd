ad7bb60395eb9f8d43d1b473fc617b34105ced15
curiosity29 <domanhdung08042001@gmail.com>
2025-02-20 17:20:39 +0700

    fireball (inprogress 2)

--- Global/Database.gd
+++ Global/Database.gd
@@ -7,4 +7,5 @@ extends Node2D
 var game_object_scenes: Dictionary[String, PackedScene] = {
 	"earth_ball": preload("res://Resource/GameObject/Summon/EarthBall/earth_ball.tscn"),
 	"earth_platform": preload("res://Resource/GameObject/Summon/earth_platform/creating_earth_platform.tscn")
+	"fire_ball": preload("res://Resource/GameObject/Summon/FireBall/fire_ball.tscn")
 }
