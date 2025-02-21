266141a4db51380ba7de9b60102a8cb7a53a7ce7
curiosity29 <dung.dm195860@sis.hust.edu.vn>
2025-02-22 01:29:51 +0700

    readd player hitbox and remove supposed debug

--- Resource/GameObject/Enemy/Goblin/goblin.gd
+++ Resource/GameObject/Enemy/Goblin/goblin.gd
@@ -3,10 +3,12 @@ extends "res://Scene/Base/enemy.gd"
 @export var animated: AnimatedSprite2D
 
 func _process(_delta: float) -> void:
-	if global_position.distance_to(player.global_position) < 100:
-		print("can attack")
-		attack()
+	pass
+	## this is just for debugging right?
+	#if global_position.distance_to(player.global_position) < 100:
+		#print("can attack")
+		#attack()
 
 func attack() -> void:
 	animated.play("attack_right")
-    
+	
