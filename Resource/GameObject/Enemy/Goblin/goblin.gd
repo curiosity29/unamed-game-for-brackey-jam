d3401644343fec55f86569fa585ee2d5fb4d0e95
XXX <julkaszot2009@gmail.com>
2025-02-21 18:59:00 +0100

    The goblin will damage you, but not the wizard

--- Resource/GameObject/Enemy/Goblin/goblin.gd
+++ Resource/GameObject/Enemy/Goblin/goblin.gd
@@ -1,2 +1,12 @@
-extends   "res://Scene/Base/enemy.gd"
+extends "res://Scene/Base/enemy.gd"
 
+@export var animated: AnimatedSprite2D
+
+func _process(_delta: float) -> void:
+	if global_position.distance_to(player.global_position) < 100:
+		print("can attack")
+		attack()
+
+func attack() -> void:
+	animated.play("attack_right")
+    
