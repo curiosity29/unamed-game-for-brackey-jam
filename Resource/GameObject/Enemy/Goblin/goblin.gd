21e2c71b1d6e22a0548aac4aee2120599be01168
XXX <julkaszot2009@gmail.com>
2025-02-22 17:20:30 +0100

    a goblin with good animation

--- Resource/GameObject/Enemy/Goblin/goblin.gd
+++ Resource/GameObject/Enemy/Goblin/goblin.gd
@@ -4,24 +4,34 @@ var left_or_right: int
 @export var animated: AnimatedSprite2D
 @export var damage_left: Area2D
 @export var damage_right: Area2D
+var is_attack: bool = false
 
 func _physics_process(_delta: float) -> void:
-	print(velocity.x)
-	if global_position.distance_to(player.global_position) < 100:
+	
+	if global_position.distance_to(player.global_position) < 180:
 		attack()
+		is_attack = true
+	else:
+		is_attack = false
 	if global_position.x > player.global_position.x:
 		left_or_right = 1
-		animated.play("walk_left")
-		damage_left.visible = true
-		damage_right.visible = false
+		if not is_attack:
+			animated.play("walk_left")
+			damage_left.visible = true
+			damage_right.visible = false
 	else:
 		left_or_right = 2
-		animated.play("walk_right")
-		damage_left.visible = false
-		damage_right.visible = true
+		if not is_attack:
+			animated.play("walk_right")
+			damage_left.visible = false
+			damage_right.visible = true
 
 func attack() -> void:
-	if left_or_right == 1:
-		animated.play("attack_right")
-	if left_or_right == 2:
-		animated.play("attack_left")
+	if is_attack:
+		if left_or_right == 2:
+			animated.play("attack_right")
+			
+		if left_or_right == 1:
+			animated.play("attack_left")
+			
+		
---
+++ Resource/GameObject/Enemy/archer/archer.gd
@@ -0,0 +1,13 @@
+extends "res://Scene/Base/enemy.gd"
+
+
+@export var animated: AnimatedSprite2D
+var reception: float = 600
+
+func _physics_process(_delta: float) -> void:
+    if global_position.distance_to(player.global_position) < 600:
+		attack()
+	
+func attack() -> void:
+    pass
+	
