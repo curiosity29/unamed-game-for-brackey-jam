b72fa5377fb7a913c33d8eeba2ffdaf35802197f
XXX <julkaszot2009@gmail.com>
2025-02-23 21:52:56 +0100

    a goblin with probably good attack (not 60 times per second)

--- Resource/GameObject/Enemy/Goblin/goblin.gd
+++ Resource/GameObject/Enemy/Goblin/goblin.gd
@@ -1,35 +1,52 @@
 extends "res://Scene/Base/enemy.gd"
-
+@onready var attack_timer = $Timer
 var left_or_right: int 
 @export var animated: AnimatedSprite2D
 @export var damage_left: Area2D
 @export var damage_right: Area2D
 var is_attack: bool = false
+var can_attack: bool = true
+
+func _ready():
+	attack_timer.timeout.connect(_on_timer_timeout)
+
+	attack_timer.start()
+func _on_timer_timeout():
+	can_attack = true
 
 func _physics_process(_delta: float) -> void:
 	
 	if global_position.distance_to(player.global_position) < 180:
-		attack()
-		is_attack = true
+		if can_attack:
+			attack()
+			can_attack = false
+			is_attack = true
+		else:
+			damage_right.monitoring = false
+			damage_left.monitoring = false
 	else:
 		is_attack = false
 	if global_position.x > player.global_position.x:
 		left_or_right = 1
 		if not is_attack:
 			animated.play("walk_left")
-			damage_left.visible = true
-			damage_right.visible = false
+			
 	else:
 		left_or_right = 2
 		if not is_attack:
 			animated.play("walk_right")
-			damage_left.visible = false
-			damage_right.visible = true
+			
 
 func attack() -> void:
-	if is_attack:
-		if left_or_right == 2:
-			animated.play("attack_right")
+	print("attack")
+	
+	if left_or_right == 2:
+		damage_right.monitoring = true
+		animated.play("attack_right")
+		
+		
 			
-		if left_or_right == 1:
-			animated.play("attack_left")
+	if left_or_right == 1:
+		damage_left.monitoring = true
+		animated.play("attack_left")
+		
