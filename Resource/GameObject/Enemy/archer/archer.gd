a6ae05fdbb97d76aaa80a1db502d2519023cef56
XXX <julkaszot2009@gmail.com>
2025-03-02 22:44:30 +0100

    archer attack animated

--- Resource/GameObject/Enemy/archer/archer.gd
+++ Resource/GameObject/Enemy/archer/archer.gd
@@ -1,6 +1,7 @@
 extends "res://Scene/Base/enemy.gd"
 
 @onready var attack_timer = $Timer
+@onready var animation_timer = $Timer_animation
 var left_or_right: int 
 @export var animated: AnimatedSprite2D
 
@@ -11,9 +12,16 @@ func _ready():
 	super()
 	attack_timer.timeout.connect(_on_timer_timeout)
 	attack_timer.start()
+	animated.frame_changed.connect(_on_animation_frame_changed)
+
+
 
 func _on_timer_timeout():
 	can_attack = true
+
+func _on_timer_animation_timeout():
+	
+	is_attack = false
 
 func _physics_process(_delta: float) -> void:
 	if global_position.distance_to(player.global_position) < 800:
@@ -33,14 +41,26 @@ func _physics_process(_delta: float) -> void:
 		left_or_right = 2
 		if not is_attack:
 			animated.play("walk_right")
+	
+		
 
 func attack() -> void:
+	
 	velocity = Vector2.ZERO
 	
 	if left_or_right == 2:
 		animated.play("attack_right")
-		add_child(load("res://Resource/GameObject/Enemy/archer/arrow/arrow_right.tscn").instantiate())
+		#add_child(load("res://Resource/GameObject/Enemy/archer/arrow/arrow_right.tscn").instantiate())
 	elif left_or_right == 1:
 		animated.play("attack_left")
-		add_child(load("res://Resource/GameObject/Enemy/archer/arrow/arrow_left.tscn").instantiate())
+		#add_child(load("res://Resource/GameObject/Enemy/archer/arrow/arrow_left.tscn").instantiate())
+	animation_timer.start()
 	
+func _on_animation_frame_changed():
+	
+	if animated.animation == "attack_left" and animated.frame == 5:
+		add_child(load("res://Resource/GameObject/Enemy/archer/arrow/arrow_left.tscn").instantiate())
+	elif animated.animation  == "attack_right" and animated.frame == 5:
+		add_child(load("res://Resource/GameObject/Enemy/archer/arrow/arrow_right.tscn").instantiate())
+		
+
